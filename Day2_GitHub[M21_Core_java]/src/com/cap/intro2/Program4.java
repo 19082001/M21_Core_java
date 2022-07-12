f1b28c4e34e3d2ddd1c3a9ee4c586927d06509e1
DELL <DELL@DESKTOP-MJ6JMGF>
2022-07-12 13:32:45 +0530

    fifth Commit

---
+++ Day2_GitHub[M21_Core_java]/src/com/cap/intro2/Program4.java
@@ -0,0 +1,12 @@
+package com.cap.intro2;
+
+import java.util.Scanner;
+
+public class Program4 {
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		String str=s.next();
+		System.out.println("The String is:"+str);
+		
+	}
+}
---
+++ Day2_GitHub[M21_Core_java]/src/com/cap/intro2/program3.java
@@ -0,0 +1,9 @@
+package com.cap.intro2;
+
+public class program3 {
+       public static void main(String[] args) {
+    	   int a=15;
+    	   System.out.println("The value of a is: "+a);
+    	   System.out.println("Welcome To M21-C2TC Program");
+       }
+}
