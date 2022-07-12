f8871263b383b823f94b857103e0184b81391981
DELL <DELL@DESKTOP-MJ6JMGF>
2022-07-12 11:16:02 +0530

    First Commit

--- Day1_GitHub/src/com/capgemini/intro/Program1.java
+++ Day1_GitHub/src/com/capgemini/intro/Program1.java
@@ -2,9 +2,11 @@ package com.capgemini.intro;
 
 public class Program1 {
 
-	public static void main(String[] args) {
-		System.out
+	public static void main (String[] args) {
+		System.out.println("Welcome To M21-C2TC Program");
 
 	}
 
 }
+
+
