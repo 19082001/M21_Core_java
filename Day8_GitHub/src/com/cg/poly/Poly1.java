eb05174ebd33d6e00fd9cccfc76a28dc9702e2fb
DELL <DELL@DESKTOP-MJ6JMGF>
2022-07-28 01:02:59 +0530

    First Commit

--- Day8_GitHub/src/com/cg/poly/Poly1.java
+++ Day8_GitHub/src/com/cg/poly/Poly1.java
@@ -1,9 +1,22 @@
 package com.cg.poly;
-
+//compiletime program
+class A
+{
+	void add(int a,int b)
+	{
+		System.out.println(a+b);
+	}
+	void add(float a,float b)
+	{
+		System.out.println(a+b);
+	}
+}
 public class Poly1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		A obj = new A();
+		obj.add(2, 5);
+		obj.add(3.0f, 4.2f);
 
 	}
 
